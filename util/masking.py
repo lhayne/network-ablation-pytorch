159b3815baced5e400f1ae90f8566857621d9905
Lucas <lhayne@nrel.gov>
2023-01-25 14:36:00 -0700

    First commit with bert demo using forward hook.

---
+++ util/masking.py
@@ -0,0 +1,57 @@
+import torch
+
+class HookedModel(torch.nn.Module):
+    """
+    Constructs a model for applying forward hooks.
+
+    Interface:
+        1. Choose model
+        2. Choose layer
+        3. Choose mask
+        4. Apply mask
+        5. Evaluate model
+        6. Remove mask
+    """
+    def __init__(self,model):
+        super(HookedModel,self).__init__()
+        self.model = model
+        self.hooks = []
+    
+    def forward(self,**kwargs):
+        return self.model(**kwargs)
+
+    def apply_hook(self,layer_name,hook):
+        self.hooks.append(
+            get_module(self.model,layer_name).register_forward_hook(hook)
+        )
+
+    def remove_hooks(self):
+        for _ in range(len(self.hooks)):
+            hook = self.hooks.pop()
+            hook.remove()
+
+
+class OutputMaskHook:
+    """
+    Hook for applying elementwise mask to output of layer.
+    """
+    def __init__(self,mask):
+        self.mask = mask
+
+    def __call__(self,model, input, output):
+        output = torch.mul(output, self.mask) # Elementwise multiplication
+        return output
+
+
+def get_module(model, name):
+    """
+    Finds the named module within the given model.
+
+    Courtesy of https://github.com/kmeng01/rome/blob/main/util/nethook.py#L355
+    """
+    for n, m in model.named_modules():
+        if n == name:
+            return m
+    raise LookupError(name)
+
+
